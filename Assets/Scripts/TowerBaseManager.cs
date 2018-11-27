6ca91314720d78c1d67930652b13925b52052da9
KeeganJennings <34640514+KeeganJennings@users.noreply.github.com>
2018-11-27 11:03:55 -0600

    This update adds the code to actually make the map appear properly

--- Assets/Scripts/TowerBaseManager.cs
+++ Assets/Scripts/TowerBaseManager.cs
@@ -6,33 +6,55 @@ public class TowerBaseManager : Singleton<TowerBaseManager>
 {
     private List<GameObject> towerBases = new List<GameObject>();
     public GameObject clickedTower;
+
+    private TextAsset mapLayout;
+    private string mapLayoutText;
+
     [SerializeField]
-    private GameObject startingBlock;
+    private GameObject[] startingBlock = new GameObject[5];
+
    
     // Use this for initialization
     void Start ()
     {
+        mapLayout = (TextAsset)Resources.Load("LevelLayout", typeof(TextAsset));
+        mapLayoutText = mapLayout.text;
+        mapLayoutText = mapLayoutText.Replace("\n", ".");
         CreateBases();
     }
 
     private void CreateBases()
     {
-        float tileSize = startingBlock.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        float tileSize = startingBlock[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
         Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
-        for (int i = 0; i < 18; i++)
+        int length = 0;
+        int height = 0;
+
+        for(int i = 0; i < mapLayoutText.Length; i++)
         {
-            for (int j = 0; j < 8; j++)
+            if(mapLayoutText[i] == '0' || mapLayoutText[i] == '1' || mapLayoutText[i] == '2' || mapLayoutText[i] == '3' || mapLayoutText[i] == '4')
             {
-                PlaceTiles(tileSize, i, j, worldStart);
+                Debug.Log(mapLayoutText[i]);
+                string s = mapLayoutText[i].ToString();
+                int tile = int.Parse(s);
+                PlaceTiles(tileSize, length, height, worldStart, tile);
+
+                length++;
+                if(length >= 17)
+                {
+                    height++;
+                    length = 0;
+                }
             }
         }
     }
 
-    private void PlaceTiles(float tileSize, int i, int j, Vector3 WorldStart)
+    private void PlaceTiles(float tileSize, int i, int j, Vector3 WorldStart, int placeTile)
     {
-        GameObject towerBase = Instantiate(startingBlock);
+        GameObject towerBase = Instantiate(startingBlock[placeTile]);
         towerBase.transform.position = new Vector3(WorldStart.x + (tileSize * i), WorldStart.y - (tileSize * j), 0);
         towerBases.Add(towerBase);
+        towerBase.transform.parent = transform;
     }
 
     // Update is called once per frame
